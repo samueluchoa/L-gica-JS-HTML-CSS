3cbbf125d9d8ffa4a789dd9b9538dcb5095316fc
Samuel-U-Silva <samukaibr@gmail.com>
2023-12-20 18:53:39 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+//alert("Este é um alerta(Pop Up ?), fica de olho hein ? -_-");
+media = (10 + 20 + 30)/3;
+document.write("A média das idades de 10, 20 e 30 anos é:______ ");
+document.write(media, " anos");
